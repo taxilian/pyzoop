8814aa76f530190b28aa333b6de9c44c5cf245be
Richard Bateman <taxilian@gmail.com>
2011-03-22 22:57:37 -0600

    Added login_required decorator

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 
 from zone import zone
 from ZincError import *
+
+from django.contrib.auth.decorators import login_required as django_login_required
+
+def login_required(f):
+   def page_func(self, request, pathList):
+       @django_login_required
+       def inner(request):
+           return f(self, request, pathList)
+       return inner(request)
+   return page_func
+
